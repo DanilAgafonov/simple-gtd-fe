7a12346c7469c5d8fc3c51215e99e422cba4465c
Danil Agafonov <mail@live-notes.ru>
2017-10-27 18:04:45 +0300

    respond with index.html for any URL

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const path = require('path');
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendfile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(process.env.PORT || 3000);
